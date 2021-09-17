ef27041960ded88c8b1f6d9d5b5aaa3e27764366
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-09-17 14:50:45 +0530

    Updated solution

--- callback6.js
+++ callback6.js
@@ -1,13 +1,17 @@
 function callback6(callback1, callback2, callback3, boards, lists, cards) {
     setTimeout(() => {
         let result = boards.find((element) => element.name === "Thanos");
+        let cardListId=[];
+
         callback1(result.id, boards, (error, result) => {
             if (result) {
                 console.log(result);
                 callback2(result.id, lists, (error, result) => {
                     if (result) {
                         console.log(result);
-                        result.forEach((list) => {
+                        result.forEach((element) => cardListId.push(element));
+                        cardListId.pop();
+                        cardListId.forEach((list) => {
                             callback3(list.id, cards, (error, result) => {
                                 if (result) {
                                     console.log(result);
