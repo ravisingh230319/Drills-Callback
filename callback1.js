8974e0541680997956f995dc3ed78349782d3fe0
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-09-21 12:43:56 +0530

    Problem statement added with Promises Solution

--- callback1.js
+++ callback1.js
@@ -1,3 +1,6 @@
+/*Problem 1: Write a function that will return a particular board's information based on the boardID that 
+is passed from the given list of boards in boards.json and then pass control back to the code that called
+it by using a callback function.*/
 function callback1(boardId, boards) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
