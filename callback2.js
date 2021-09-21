94d41d759c67cf6a35d619b00cece2e7a8c15818
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-09-21 12:51:44 +0530

    Problem statement added with Callback Solution

--- callback2.js
+++ callback2.js
@@ -1,3 +1,6 @@
+/*Problem 2: Write a function that will return all lists that belong to a board based on the boardID that is
+passed to it from the given data in lists.json. Then pass control back to the code that called it by using a
+callback function.*/
 function callback2(boardId,lists,cb){
 
     setTimeout(() => {      
--- callback3.js
+++ callback3.js
@@ -1,3 +1,6 @@
+/*Problem 3: Write a function that will return all cards that belong to a particular list based on the listID
+ that is passed to it from the given data in cards.json. Then pass control back to the code that called it by
+ using a callback function.*/
 function callback3(listId,cards,cb){
 
     setTimeout(() => {     
